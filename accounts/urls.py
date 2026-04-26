4358be1e75e25b227a6eac309ae21823eaa8c80c
amen <amenallahjayari04@gmail.com>
2026-04-26 19:43:41 +0200

    Initial commit - BloodConnect project

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('dashboard/donneur/', views.donneur_dashboard, name='donneur_dashboard'),
+]
